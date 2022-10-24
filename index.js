99860dc082e07776fbd7e6b7748d24e81cf93fbe
Rocko <codingwithrock@gmail.com>
2022-10-24 23:43:00 +0600

    added cors

--- index.js
+++ index.js
@@ -3,31 +3,34 @@ const app = express();
 const phones = require('./phones.json');
 const port = 5000;
 
+// added cors.
+const cors = require('cors');
+app.use(cors());
 
 
 
 app.get('/', (req, res) => {
-    res.send("hello world from node!!2!!") 
+    res.send("hello world from node!!2!!")
 });
 
 
-app.get('/phones', (req, res)=> {
+app.get('/phones', (req, res) => {
     res.send(phones);
 });
 
 
 
-app.get('/phones/:id',(req,res)=> {
+app.get('/phones/:id', (req, res) => {
 
     const id = parseInt(req.params.id);
-    const phone = phones.find(ph=> ph.id === id) || {}
+    const phone = phones.find(ph => ph.id === id) || {}
     res.send(phone);
-    
+
 
 })
 
 
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Server Running on port ${port}`)
 });
